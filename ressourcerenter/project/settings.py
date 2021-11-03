68ed5fb6fa86e24e88ffaade92b31420e528b76b
Lars Peter Thomsen <larsp@magenta.dk>
2021-11-03 13:24:45 +0100

    More django setup

--- ressourcerenter/project/settings.py
+++ ressourcerenter/project/settings.py
@@ -20,11 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 ENVIRONMENT = os.environ['ENVIRONMENT']
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-
 DEBUG = strtobool(os.environ.get('DJANGO_DEBUG', 'False'))
 
 ALLOWED_HOSTS = ['*']
