590c34ac5443d33b9c6683b7cee70f6fffc4d445
Lars Peter Thomsen <larsp@magenta.dk>
2021-11-03 17:09:54 +0100

    Template and staticfiles setup

--- ressourcerenter/project/settings.py
+++ ressourcerenter/project/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ 'project/templates' ],
+        'DIRS': [
+            'project/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
