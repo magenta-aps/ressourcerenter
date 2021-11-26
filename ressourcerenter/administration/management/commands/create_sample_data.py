bd2e9046e603c30c0c6c3f74fae8d4e91d611fcc
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2021-11-26 14:49:38 +0100

    Resolve #47159 "Feature/"

--- ressourcerenter/administration/management/commands/create_sample_data.py
+++ ressourcerenter/administration/management/commands/create_sample_data.py
@@ -1,8 +1,9 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth import get_user_model
 from django.conf import settings
-from indberetning.models import Indberetning, Virksomhed, ProduktKategori, IndberetningLinje
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
 from administration.models import Afgiftsperiode, FiskeArt, Kvartal
+from indberetning.models import Indberetning, Virksomhed, ProduktKategori, IndberetningLinje
 
 
 class Command(BaseCommand):
@@ -21,27 +22,38 @@ class Command(BaseCommand):
         kvartal1, _ = Kvartal.objects.get_or_create(aar=2021, kvartal=4)
         kvartal2, _ = Kvartal.objects.get_or_create(aar=2021, kvartal=3)
 
-        afgiftsperiode1, _ = Afgiftsperiode.objects.get_or_create(navn='4. kvartal 2021', vis_i_indberetning=True, aarkvartal=kvartal1)
+        afgiftsperiode1, _ = Afgiftsperiode.objects.get_or_create(navn='4. kvartal 2021',
+                                                                  vis_i_indberetning=True,
+                                                                  aarkvartal=kvartal1)
+        afgiftsperiode2, _ = Afgiftsperiode.objects.get_or_create(navn='3. kvartal 2021',
+                                                                  vis_i_indberetning=True,
+                                                                  aarkvartal=kvartal2)
+        kategorier = []
+        for kategori in ('Hel fisk', 'Filet', 'Biprodukt'):
+            kat, _ = ProduktKategori.objects.get_or_create(navn=kategori)
+            kategorier.append(kat)
 
-        afgiftsperiode2, _ = Afgiftsperiode.objects.get_or_create(navn='3. kvartal 2021', vis_i_indberetning=True, aarkvartal=kvartal2)
         reje, _ = FiskeArt.objects.get_or_create(navn='reje')
         torsk, _ = FiskeArt.objects.get_or_create(navn='Torsk')
-        ProduktKategori.objects.get_or_create(navn='Hel fisk')
-        ProduktKategori.objects.get_or_create(navn='Filet')
-        ProduktKategori.objects.get_or_create(navn='Bi produkt')
         virksomhed, _ = Virksomhed.objects.get_or_create(cvr='12345678')
-
         if not Indberetning.objects.exists():
             for periode in (afgiftsperiode1, afgiftsperiode2):
-                indberetning = Indberetning.objects.create(virksomhed=virksomhed,
-                                                           afgiftsperiode=periode,
-                                                           navn='Bygd for indhandling',
-                                                           indberetnings_type='indhandling',
-                                                           indberetters_cpr='123456-1955')
+                for i, indberetnings_type in enumerate(('indhandling', 'pelagisk', 'fartøj')):
+                    if indberetnings_type == 'indhandling':
+                        navn = 'Bygd for indhandling'
+                        kategori = None
+                    else:
+                        navn = 'Tidligere fartøj'
+                        kategori = kategorier[i]
+                    indberetning = Indberetning.objects.create(virksomhed=virksomhed,
+                                                               afgiftsperiode=periode,
+                                                               navn=navn,
+                                                               indberetnings_type=indberetnings_type,
+                                                               indberetters_cpr='123456-1955')
 
-                IndberetningLinje.objects.create(indberetning=indberetning,
-                                                 salgs_vægt=10,
-                                                 levende_vægt=20,
-                                                 salgs_pris=400,
-                                                 fiskeart=torsk,
-                                                 )
+                    IndberetningLinje.objects.create(indberetning=indberetning,
+                                                     salgsvægt=10,
+                                                     levende_vægt=20,
+                                                     salgspris=400,
+                                                     kategori=kategori,
+                                                     fiskeart=FiskeArt.objects.get(navn='Torsk'))
