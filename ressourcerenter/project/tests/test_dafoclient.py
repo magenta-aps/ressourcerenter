ae608ebef5cd5fd755b6b100c273c52f0f3c23e1
Mads Møller Johansen <mmj@magenta.dk>
2021-11-26 14:41:22 +0100

    47141: refactor

--- ressourcerenter/project/tests/test_dafoclient.py
+++ ressourcerenter/project/tests/test_dafoclient.py
@@ -3,13 +3,11 @@ from django.test import TestCase, override_settings
 from project.dafo import DatafordelerClient
 
 
-@override_settings(OPENID={'mock': 'cvr'})
 class TestDafoConnection(TestCase):
 
     # sudo apt install curl
     # curl -k --key /etc/ssl/pitu/key.pem --cert /etc/ssl/pitu/certificate.crt --header "Uxp-client: PITU/GOV/DIA/magenta_services" --header "Uxp-service: PITU/GOV/DIA/magenta_services/DAFO-PRISME-CVR/v1" "https://10.240.76.91/restapi/12950160"
     def test_lookup_company_name(self):
-        pass
         """
         Call the dafo-client to get a companyinfo
         """
