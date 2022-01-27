8982f2a7cb5d5804c097488aa9d62358c6db4e31
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2022-01-27 14:15:12 +0100

    add debugging

--- ressourcerenter/indberetning/views.py
+++ ressourcerenter/indberetning/views.py
@@ -22,13 +22,15 @@ from indberetning.forms import IndberetningsTypeSelectForm, VirksomhedsAddressFo
     IndberetningsLinjeBeregningForm, IndberetningSearchForm
 from indberetning.models import Indberetning, Virksomhed, IndberetningLinje, Bilag
 from project.dafo import DatafordelerClient
-
+import logging
+logger = logging.getLogger(__name__)
 LoginProvider = import_string(settings.LOGIN_PROVIDER_CLASS)
 
 
 class Frontpage(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         cvr = self.request.session.get('cvr')
+        logger.exception('Frontpage: cvr: %s' % cvr)
         if cvr:
             # lookup company information
             dafo_client = DatafordelerClient.from_settings()
@@ -361,7 +363,9 @@ class LoginView(View):
 class LoginCallbackView(View):
     def get(self, request):
         provider = LoginProvider.from_settings()
-        if provider.handle_login_callback(request=request):
+        r = provider.handle_login_callback(request=request)
+        logger.exception('handle_login_callback %s' % r)
+        if r:
             # if the call back was successfully, redirect to frontpage
             return HttpResponseRedirect(reverse('indberetning:frontpage'))
         return HttpResponseRedirect(reverse('indberetning:login'))
