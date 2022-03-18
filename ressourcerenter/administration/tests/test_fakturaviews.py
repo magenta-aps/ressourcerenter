e54a36e0cdffd01197b2bbd8a7920d0fdf65cccc
Lars Peter Thomsen <larsp@magenta.dk>
2022-03-18 10:39:19 +0100

    Test fakturacreateview

--- ressourcerenter/administration/tests/test_fakturaviews.py
+++ ressourcerenter/administration/tests/test_fakturaviews.py
@@ -1,16 +1,16 @@
 from administration.management.commands.create_initial_dataset import Command as CreateInitialDatasetCommand
-from administration.models import Afgiftsperiode, FiskeArt, ProduktType, SkemaType, Faktura
+from administration.models import Afgiftsperiode, ProduktType, SkemaType, Faktura
+from administration.models import BeregningsModel2021
+from administration.models import Prisme10QBatch
 from datetime import date
 from decimal import Decimal
-from django.contrib.auth import get_user_model
-from django.test import TransactionTestCase
-from indberetning.models import Virksomhed, Indberetning, IndberetningLinje
-from django.urls import reverse
-from django.contrib.auth.models import Group
-
-from django.test import override_settings
-from administration.models import Prisme10QBatch
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.test import TransactionTestCase
+from django.test import override_settings
+from django.urls import reverse
+from indberetning.models import Virksomhed, Indberetning, IndberetningLinje
 from unittest.mock import patch
 
 
@@ -28,7 +28,32 @@ class PrismeTestCase(TransactionTestCase):
 
         self.skematyper = {s.id: s for s in SkemaType.objects.all()}
         self.virksomhed = Virksomhed.objects.create(cvr=1234)
-        self.periode = Afgiftsperiode.objects.create(navn_dk='testperiode', dato_fra=date(2022, 1, 1), dato_til=date(2022, 3, 31))
+        self.beregningsmodel = BeregningsModel2021.objects.create(navn='TestBeregningsModel')
+        self.periode = Afgiftsperiode.objects.create(beregningsmodel=self.beregningsmodel, navn_dk='testperiode', dato_fra=date(2022, 1, 1), dato_til=date(2022, 3, 31))
+
+
+    @override_settings(PRISME_PUSH={**settings.PRISME_PUSH, 'do_send': False})
+    @patch.object(Prisme10QBatch, 'completion_statuses', {
+        '10q_production': Prisme10QBatch.STATUS_DELIVERED,
+        '10q_development': Prisme10QBatch.STATUS_DELIVERED
+    })
+    def test_fakturacreateview(self):
+        CreateInitialDatasetCommand().handle()
+
+        indberetning = Indberetning.objects.create(afgiftsperiode=self.periode, skematype=self.skematyper[1], virksomhed=self.virksomhed)
+        linje = IndberetningLinje.objects.create(indberetning=indberetning, produkttype=ProduktType.objects.get(navn_dk='Makrel, ikke-grønlandsk fartøj'), levende_vægt=1000, salgspris=10000)
+
+        response = self.client.post(reverse('administration:faktura-create', kwargs={'pk': linje.pk}), {'betalingsdato': '2022-03-18'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('administration:login') + "?next=" + reverse('administration:faktura-create', kwargs={'pk': linje.pk}))
+
+        self.assertTrue(self.client.login(username=self.username, password=self.password))
+        response = self.client.post(reverse('administration:faktura-create', kwargs={'pk': linje.pk}), {'betalingsdato': '2022-03-18'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('administration:indberetningslinje-list'))
+
+        linje.refresh_from_db()
+        self.assertEqual(linje.faktura.batch.status, 'delivered')
 
 
     @override_settings(PRISME_PUSH={**settings.PRISME_PUSH, 'do_send': False})
