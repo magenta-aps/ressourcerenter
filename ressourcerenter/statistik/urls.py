a1ac9d33d302dd1746c2464db336ac43bcf3c9d7
Lars Peter Thomsen <larsp@magenta-aps.dk>
2022-06-02 11:02:08 +0200

    [#50627] form filter

--- ressourcerenter/statistik/urls.py
+++ ressourcerenter/statistik/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from statistik.apps import StatistikConfig
-from statistik.views import StatistikView
+from statistik.views import StatistikView, StatistikChoicesView
 
 app_name = StatistikConfig.name
 
 urlpatterns = [
     path('', StatistikView.as_view(), name='frontpage'),
+    path('choices', StatistikChoicesView.as_view(), name='choices'),
 ]
