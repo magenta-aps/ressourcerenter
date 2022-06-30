1d4c58c636fae6c8fc5e39d052c011e6ad1ca5d8
Lars Peter Thomsen <larsp@magenta-aps.dk>
2022-06-30 09:47:18 +0200

    [#51075] Thousands-separator on clientside

--- ressourcerenter/project/form_fields.py
+++ ressourcerenter/project/form_fields.py
@@ -5,9 +5,13 @@ from django.utils.translation import gettext as _
 
 
 class LocalizedDecimalField(forms.DecimalField):
-    def __init__(self, max_value=None, min_value=None, max_digits=None, decimal_places=None, **kwargs):
-        kwargs['widget'] = TextInput  # use text input to avoid input type=number
+    def __init__(self, max_value=None, min_value=None, max_digits=None, decimal_places=None, clientside_formatting=False, **kwargs):
+        attrs = {}
+        if clientside_formatting:
+            attrs['data-input-tsep'] = 'true'
+        kwargs['widget'] = TextInput(attrs=attrs)  # use text input to avoid input type=number
         kwargs['localize'] = True  # allow localized values such as 1.200,24
+        self.clientside_formatting = clientside_formatting
         super(LocalizedDecimalField, self).__init__(max_value=max_value,
                                                     min_value=min_value,
                                                     max_digits=max_digits,
@@ -15,8 +19,12 @@ class LocalizedDecimalField(forms.DecimalField):
                                                     **kwargs)
 
     def to_python(self, value):
-        if '.' in str(value):
-            raise ValidationError(_('Tallet må ikke indeholde punktum, kun komma'), code='invalid')
+        if self.clientside_formatting:
+            if value is not None:
+                value = value.replace('.', '')
+        else:
+            if '.' in str(value):
+                raise ValidationError(_('Tallet må ikke indeholde punktum, kun komma'), code='invalid')
         return super().to_python(value)
 
 
