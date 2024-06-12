28d537c3ce6c1314098d6bbbf8482c8283e75941
Lars Peter Thomsen <larsp@magenta.dk>
2024-06-12 12:29:53 +0200

    feat: [#60873] CSP

--- ressourcerenter/project/urls.py
+++ ressourcerenter/project/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path("", include("django_mitid_auth.urls", namespace="login")),
     path("_ht/", include("watchman.urls")),
     path("i18n/", include("django.conf.urls.i18n")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
