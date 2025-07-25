e465ac42bc56320612788e37e62a3d7b58888de8
Peter Juelsgaard <pj@magenta.dk>
2025-07-25 09:34:15 +0200

    fix: [#65762] Include warnings if user inputs more than 2 decimal places

--- ressourcerenter/indberetning/forms.py
+++ ressourcerenter/indberetning/forms.py
@@ -75,27 +75,34 @@ class IndberetningsLinjeBeregningForm(ModelForm):
         clientside_formatting=True,
         max_value=999_999_999,
         min_value=-999_999_999,
+        decimal_places=2,
     )
     levende_vægt = LocalizedDecimalField(
-        clientside_formatting=True, max_value=999_999_999, min_value=-999_999_999
+        clientside_formatting=True,
+        max_value=999_999_999,
+        min_value=-999_999_999,
+        decimal_places=2,
     )
     salgspris = LocalizedDecimalField(
         required=False,
         clientside_formatting=True,
         max_value=999_999_999,
         min_value=-999_999_999,
+        decimal_places=2,
     )
     transporttillæg = LocalizedDecimalField(
         required=False,
         clientside_formatting=True,
         max_value=999_999_999,
         min_value=-999_999_999,
+        decimal_places=2,
     )
     bonus = LocalizedDecimalField(
         required=False,
         clientside_formatting=True,
         max_value=999_999_999,
         min_value=-999_999_999,
+        decimal_places=2,
     )
 
     def clean(self):
@@ -275,6 +282,7 @@ class IndberetningsLinjeSkema1Form(NonPelagiskPrisRequired, IndberetningsLinjeFo
         clientside_formatting=True,
         max_value=999_999_999,
         min_value=-999_999_999,
+        decimal_places=2,
     )
     fartøj_navn = CharField(
         widget=Select(
@@ -322,12 +330,14 @@ class IndberetningsLinjeSkema2Form(NonPelagiskPrisRequired, IndberetningsLinjeFo
         clientside_formatting=True,
         max_value=999_999_999,
         min_value=-999_999_999,
+        decimal_places=2,
     )
     produktvægt = LocalizedDecimalField(
         required=True,
         clientside_formatting=True,
         max_value=999_999_999,
         min_value=-999_999_999,
+        decimal_places=2,
     )
     fartøj_navn = CharField(
         widget=Select(
@@ -372,6 +382,7 @@ class IndberetningsLinjeSkema3Form(IndberetningsLinjeForm):
         clientside_formatting=True,
         max_value=999_999_999,
         min_value=-999_999_999,
+        decimal_places=2,
     )
     indhandlingssted = ModelChoiceField(
         queryset=Indhandlingssted.objects.filter(aktiv_til_indhandling=True)
